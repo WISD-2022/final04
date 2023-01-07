0db5165706a2af5dc2200701dba2ed4573c65f68
3A932010 <jack37699646@gmail.com>
2023-01-07 21:55:32 +0800

    搜尋產品功能

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,12 +75,8 @@
                     <div class="logo logo-width-1">
                         <a href="index.html"><img src="assets/imgs/logo/logo.png" alt="logo"></a>
                     </div>
-                    <div class="header-right">
-                        <div class="search-style-1">
-                            <form action="#">
-                                <input type="text" placeholder="Search for items...">
-                            </form>
-                        </div>
+                    <div class="header-right">  
+                        @livewire('header-search-component')                 
                         <div class="header-action-right">
                             <div class="header-action-2">
                                 <div class="header-action-icon-2">
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\DetailsComponent;
+use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use Illuminate\Support\Facades\Route;
@@ -37,6 +38,7 @@ Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 Route::get('/checkout', CheckoutComponent::class)->name('shop.checkout');
 
 Route::get('/product-category/{slug}',CategoryComponent::class)->name('product.category');
+Route::get('/search',SearchComponent::class)->name('product.search');
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
