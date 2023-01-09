d83d8fd81264f60ba77999bf3ad509808df19e5d
ArTim <3a932118@gm.student.ncut.edu.tw>
2023-01-09 13:03:41 +0800

    管理者刪除產品類別

--- app/Http/Livewire/Admin/AdminCategoriesComponent.php
+++ app/Http/Livewire/Admin/AdminCategoriesComponent.php
@@ -8,10 +8,19 @@ use Livewire\WithPagination;
 
 class AdminCategoriesComponent extends Component
 {
+    public $category_id;
+
     use WithPagination;
+
+    public function deleteCategory($id){
+        $category = Category::find($id);
+        $category->delete();
+        session()->flash('message', '刪除成功');
+    }
+
     public function render()
     {
-        $categories = Category::orderBy('id', 'ASC')->paginate(5);
+        $categories = Category::orderBy('name', 'ASC')->paginate(5);
         return view('livewire.admin.admin-categories-component', ['categories'=>$categories]);
     }
 }
--- resources/views/livewire/admin/admin-categories-component.blade.php
+++ resources/views/livewire/admin/admin-categories-component.blade.php
@@ -1,4 +1,8 @@
 <div>
+    <head>
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
     <style>
         nav svg{
             height: 20px;
@@ -33,20 +37,27 @@
                                 </div>
                             </div>
                             <div class="card-body">
+                                @if(Session::has('message'))
+                                <div class="alert alert-success" role="alert">{{Session::get('message')}}</div>
+                                @endif
                                 <table class="table table-striped">
                                     <thead>
                                         <tr>
-                                            <th>#</th>
+                                            <th>Id</th>
                                             <th>類別名稱</th>
-                                            <th>Action</th>
+                                            <th>動作</th>
                                         </tr>
                                         <tbody>
+                                            @php
+                                            	$i = ($categories->currentPage()-1)*$categories->perPage();
+                                            @endphp
                                             @foreach($categories as $category)
                                                 <tr>
-                                                    <td>{{ $category->id }}</td>
+                                                    <td>{{ ++$i }}</td>
                                                     <td>{{ $category->name }}</td>
                                                     <td>
-                                                        <a href="{{ route('admin.category.edit', ['category_id'=>$category->id])}}" class="text_info">修改</a>
+                                                        <a href="{{ route('admin.category.edit', ['category_id'=>$category->id])}}" ><i class="fa fa-edit" style="font-size:24px; color:blue;"></i></a>
+                                                        <a href="#" class="text_danger" wire:click.prevent="deleteCategory({{$category->id}})" style="margin-left:20px;"><i class="fa fa-trash" style="font-size:24px; color:red;"></i></a>
                                                     </td>
                                                 </tr>
                                             @endforeach
@@ -56,7 +67,6 @@
                                 <div>
                                     {{ $categories->links('vendor.pagination.bootstrap-4') }}
                                 </div>
-                                
                             </div>
                         </div>
                     </div>
@@ -65,3 +75,28 @@
         </scation>
     </main>
 </div>
+
+<div class="modal" id="deleteConfirmation">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body pb-30 pt-30">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <h4 class="pb-3">你確定要刪除嗎?</h4>
+                        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#deleteConfirmation">取消</button>
+                        <button type="button" class="btn btn-danger">刪除</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@push('scripts')
+    <script type="text/javascript">
+        function deleteConfirmation(id){
+            @this.set('category_id',id);
+            $('#deleteConfirmation').modal('show');
+        }
+    </script>
+@endpush
