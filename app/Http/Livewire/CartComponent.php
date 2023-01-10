72f8cf432b0bd2c28e35723604df65b957a93288
ArTim <3a932118@gm.student.ncut.edu.tw>
2023-01-11 05:27:06 +0800

    購物車燈入驗證

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Cart;
+use Illuminate\Support\Facades\Auth;
 
 class CartComponent extends Component
 {
@@ -37,8 +38,34 @@ class CartComponent extends Component
         $this->emitTo('cart-icon-component','refreshComponent');
     }
 
+    public function checkout(){
+        if(Auth::check()){
+            return redirect()->route('checkout');
+        }else{
+            return redirect()->route('login');
+        }
+    }
+
+    public function setAmountForCheckout(){
+
+        if(session()->has('coupon')){
+            session()->put('checkout',[
+                'subtotal' => $this->subtotalAfterDiscount,
+                'tax' => $this->taxAfterDiscount,
+                'total' => $this->totalAfterDiscount,
+            ]);
+        }else{
+            session()->put('checkout',[
+                'subtotal' => Cart::instance('cart')->subtotal(),
+                'tax' => Cart::instance('cart')->tax(),
+                'total' => Cart::instance('cart')->total(),
+            ]);
+        }
+    }
+
     public function render()
     {
+        $this->setAmountForCheckout();
         return view('livewire.cart-component');
     }
 }
---
+++ database/migrations/2023_01_11_024036_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->decimal('subtotal');
+            $table->decimal('tex');
+            $table->decimal('total');
+            $table->string('name');
+            $table->string('email');
+            $table->enum('status', ['ordered', 'delivered', 'canceled'])->default('ordered');
+            $table->boolean('is_shopping_different')->default(false);
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
